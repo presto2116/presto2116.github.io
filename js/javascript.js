e645188e2e0463ce88b677e10faf44e09d40db5d
presto2116 <preston.straight@gmail.com>
2015-06-16 13:45:22 -0400

    styling and bootstrap

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+   $('a[href*=#]:not([href=#])').click(function() {
+       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+         var target = $(this.hash);
+         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+         if (target.length) {
+           $('html,body').animate({
+             scrollTop: (target.offset().top - 50) // adjust this according to your content
+           }, 1000);
+           return false;
+         }
+       }
+   });
+ });
