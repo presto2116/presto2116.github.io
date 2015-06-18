08ba4cc2e3f5a85b112336f76a31cb398fb3ce76
presto2116 <preston.straight@gmail.com>
2015-06-17 21:10:11 -0400

    styling and layout

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,5 @@
 
+
 $(document).ready(function(){
    $('a[href*=#]:not([href=#])').click(function() {
        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
