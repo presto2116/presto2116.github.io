b8071883d4d2cbe2c69aadbdf143d653cbd23c3d
presto2116 <preston.straight@gmail.com>
2015-06-19 01:05:11 -0400

    added meta data

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,7 @@
 
 
 $(document).ready(function(){
-  var frameSrc = "/login";
-
-$('#openBtn').click(function(){
-    $('#myModal').on('show', function () {
-
-        $('iframe').attr("src",frameSrc);
-      
-  });
-    $('#myModal').modal({show:true})
-});
+  
    $('a[href*=#]:not([href=#])').click(function() {
        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
          var target = $(this.hash);
